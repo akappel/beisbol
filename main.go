700c2a7cee76de8698e6a85e763c14d896b0779d
akappel <atkappel@gmail.com>
2017-02-07 21:36:19 -0500

    So close!
    I might be overthinking the translation regex, so I'm going to take a
    break and look at it with a fresh set of eyes. Also: new function names!

--- main.go
+++ main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"regexp"
+	"strings"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+type translation struct {
+	term         string
+	translations []string
 }
 
 func main() {
 	// dat, err := ioutil.ReadFile("dict.txt")
 	// check(err)
-	// out, err := findDefinitionGroups(dat)
+	// out, err := findTranslationGroups(dat)
 	// check(err)
 
 	// for _, el := range out {
@@ -22,7 +24,7 @@ func main() {
 	// }
 }
 
-func findDefinitionGroups(in []byte) (out [][]byte, err error) {
+func findTranslationGroups(in []byte) (out [][]byte, err error) {
 	r, err := regexp.Compile(".+((\\n .+)*|(\\n\\(.+)*)*[^\\n]")
 	if err != nil {
 		return
@@ -32,14 +34,66 @@ func findDefinitionGroups(in []byte) (out [][]byte, err error) {
 	return
 }
 
-func findDefinitions(in []byte) (out [][]byte, err error) {
-	// remove all newlines
-	in = bytes.Replace(in, []byte("\n"), []byte(""), -1)
-	r, err := regexp.Compile(".+")
+func findTranslations(in []byte) (out []translation, err error) {
+	if len(in) < 1 {
+		err = errors.New("Length of input must be greater than 1")
+		return
+	}
+
+	termRegex, err := regexp.Compile(".+\\-")
+	translationRegex, err := regexp.Compile(".+\\.")
 	if err != nil {
 		return
 	}
 
-	out = r.FindAll(in, -1)
+	// remove all newlines
+	in = bytes.Replace(in, []byte("\n"), []byte(""), -1)
+
+	// split on semi-colons
+	splits := bytes.Split(in, []byte(";"))
+
+	for _, split := range splits {
+		if t := termRegex.Find(split); t != nil {
+			// we've found the term in our split, create a new dict struct
+			term := string(t)
+			term = strings.TrimRight(term, "-")
+			out = append(out, translation{string(t), []string{}})
+		}
+
+		if d := translationRegex.Find(split); d != nil {
+			// translation found, add it
+			translation := string(d)
+			fmt.Println(translation)
+			// add to last generated translation
+			out[len(out)-1].translations = append(out[len(out)-1].translations, translation)
+		}
+	}
+
+	// if just a single translation was found, generate the struct and return
+	// if len(splits) == 1 {
+	// 	t := termRegex.Find(splits[0])
+	// 	trans := definitionRegex.Find(splits[0])
+	//
+	// 	t = strings.TrimRight(t, "-")
+	// 	out = append(out, definition{term: t, translations: []string{}})
+	// 	fmt.Println(out[0].term)
+	// }
+
+	// for _, split := range splits {
+	// 	fmt.Println(string(split))
+	// }
+
 	return
+	// r, err := regexp.Compile(".+")
+	// if err != nil {
+	// 	return
+	// }
+	//
+	// out = r.FindAll(in, -1)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,8 +1,11 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-var findDefinitionGroupsTests = []struct {
+var findTranslationGroupsTests = []struct {
 	in  []byte
 	out [][]byte
 }{
@@ -11,16 +14,16 @@ var findDefinitionGroupsTests = []struct {
 	{[]byte("b (o)-n.f. p; b; p\n(p) m. e (s); m. c\n d c (h)"), [][]byte{[]byte("b (o)-n.f. p; b; p\n(p) m. e (s); m. c\n d c (h)")}},
 }
 
-func TestFindParentDefinitions(t *testing.T) {
-	for _, tt := range findDefinitionGroupsTests {
-		out, err := findDefinitionGroups(tt.in)
+func TestFindTranslationGroups(t *testing.T) {
+	for _, tt := range findTranslationGroupsTests {
+		out, err := findTranslationGroups(tt.in)
 		if err != nil {
 			t.Fatal(err)
 		}
 
 		// if the regex just completely failed to find anything, fail out
 		if len(out) < 1 {
-			t.Errorf("findDefinitionGroups(%q) did not find anything!", tt.in)
+			t.Errorf("findTranslationGroups(%q) did not find anything!", tt.in)
 			return
 		}
 
@@ -28,49 +31,79 @@ func TestFindParentDefinitions(t *testing.T) {
 		got := string(out[0])
 		expected := string(tt.out[0])
 		if got != expected {
-			t.Errorf("findDefinitionGroups(%q) => %q, want %q", string(tt.in), got, expected)
+			t.Errorf("findTranslationGroups(%q) => %q, want %q", string(tt.in), got, expected)
 		}
 
 	}
 }
 
-var findDefinitionsTests = []struct {
+var findTranslationsTests = []struct {
 	in  []byte
-	out [][]byte
+	out []translation
 }{
-	{[]byte("ba-n.f.adj. m li"), [][]byte{[]byte("ba-n.f.adj. m li")}},
-	{[]byte("ba-n.f.adj. m li; f"), [][]byte{[]byte("ba-n.f.adj. m li; f")}},
-	{[]byte("ba-n.f.adj. m li; f; f b-n.m. am d s"), [][]byte{[]byte("ba-n.f.adj. m li; f"), []byte("f b-n.m am d s")}},
+	{[]byte("b-a-n.f.adj. m li"), []translation{translation{"b-a", []string{"m li"}}}},
+	// {[]byte("ba-n.f.adj. m li; f"), [][]byte{[]byte("ba-n.f.adj. m li; f")}},
+	// {[]byte("ba-n.f.adj. m li; f; f b-n.m. am d s"), [][]byte{[]byte("ba-n.f.adj. m li; f"), []byte("f b-n.m am d s")}},
 }
 
-func TestFindChildDefinitions(t *testing.T) {
-	for _, tt := range findDefinitionsTests {
-		out, err := findDefinitions(tt.in)
+func TestFindTranslations(t *testing.T) {
+	for _, tt := range findTranslationsTests {
+		defs, err := findTranslations(tt.in)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		if len(out) < 1 {
-			t.Errorf("findDefinitions(%q) did not find anything!", tt.in)
+		if len(defs) < 1 {
+			t.Errorf("findTranslations(%q) did not find anything!", tt.in)
 			return
 		}
 
-		for _, ttchild := range tt.out {
-			expected := string(ttchild)
+		for _, expectedDef := range tt.out {
+			// for each expected definition, we check that output from the function has a definition struct
+			// that matches.
 			found := false
 
-			for _, child := range out {
-				got := string(child)
-				t.Logf("got: %q", got)
-
-				if got == expected {
+			for _, def := range defs {
+				if def.term == expectedDef.term && reflect.DeepEqual(def.translations, expectedDef.translations) {
 					found = true
 				}
 			}
 
 			if !found {
-				t.Errorf("findDefinitions(%q) => %q not found in output set!", string(tt.in), expected)
+				t.Errorf("findTranslations(%q) => expected definition(s) not found in output set!", string(tt.in))
 			}
 		}
 	}
 }
+
+// func TestFindDefinitions(t *testing.T) {
+// 	for _, tt := range findTranslationsTests {
+// 		out, err := findTranslations(tt.in)
+// 		if err != nil {
+// 			t.Fatal(err)
+// 		}
+//
+// 		if len(out) < 1 {
+// 			t.Errorf("findTranslations(%q) did not find anything!", tt.in)
+// 			return
+// 		}
+//
+// 		for _, ttchild := range tt.out {
+// 			expected := string(ttchild)
+// 			found := false
+//
+// 			for _, child := range out {
+// 				got := string(child)
+// 				t.Logf("got: %q", got)
+//
+// 				if got == expected {
+// 					found = true
+// 				}
+// 			}
+//
+// 			if !found {
+// 				t.Errorf("findTranslations(%q) => %q not found in output set!", string(tt.in), expected)
+// 			}
+// 		}
+// 	}
+// }
